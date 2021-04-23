a2019f29175eb8a5e5ec2428dbe1889664f8807e
Martin Ryba <martin.ryba01@gmail.com>
2021-04-23 03:08:20 +0200

    basic config setup

---
+++ client/webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: './index.html',
+    filename: './index.html'
+});
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+    // This is the "main" file which should include all other modules
+    entry: './src/main.js',
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm-bundler.js'
+        }
+    },
+    plugins: [new VueLoaderPlugin(), htmlPlugin]
+};
