f60ac739bfec9ec78d39f7fbb03273b77cf77d30
Martin Ryba <martin.ryba01@gmail.com>
2021-04-23 15:01:38 +0200

    added basic css. Configured css loaders in webpack

--- client/src/components/MainScreen.vue
+++ client/src/components/MainScreen.vue
@@ -1,6 +1,10 @@
 <template>
-<Map></Map>
-<GpxManager></GpxManager>
+    <div class="map">
+        <Map></Map>
+    </div>
+    <div class="gpx">
+        <GpxManager></GpxManager>
+    </div>
 </template>
 
 <script>
@@ -16,6 +20,22 @@ export default {
 </script>
 
 
-<style scoped>
-
+<style>
+html, body {
+    height: 100%;
+    margin: 0;
+}
+#app {
+    height: 100%;
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+}
+.map {
+    background-color: red;
+    grid-column: 1 / 1;
+}
+.gpx {
+    background-color: blue;
+    grid-column: 2 / 2;
+}
 </style>
