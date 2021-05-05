8b9e87d656a487478c55cac0eb4739a719242eed
Martin Ryba <martin.ryba01@gmail.com>
2021-05-05 17:30:59 +0200

    added fallback path in case of invalid url

--- server/index.js
+++ server/index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/hi', (req, res) => {
     res.send('Hello!');
+})
+
+// fallback option for invalid paths
+app.get(/.*/, (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
 })
 
 app.listen(port, () => console.log(`listening at port ${port}`));
